1f5f375126278d6f785ee300ae19ca12d972d5ad
Cyprian <bieglywybuch@gmail.com>
2025-06-18 08:48:40 +0200

    Added playback speed.py from CS50P course , from problem set 0

---
+++ Problem set 0/playback speed/playback.py
@@ -0,0 +1,9 @@
+variable = input()
+
+words = variable.split() #make list of words that were in variable (sorry for bad English but, I'm not native speaker)
+output = '...'.join(words)  #making ... before every word
+
+print(output)
+
+
+
